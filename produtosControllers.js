92fde6eed0cde2bc25d2cb3600b725a26c862578
senailukinhas <senailukinhas@gmail.com>
2025-06-03 15:54:30 -0300

    Add files via upload

---
+++ produtosControllers.js
@@ -0,0 +1,111 @@
+const conexao = require("../db/conexao")
+
+exports.criarProduto = (req,res) => {
+    const {nome,preco,quantidade} = req.body;
+
+    conexao.query(
+        "INSERT INTO produtos (nome,preco, quantidade) VALUES (?,?,?)",
+        [nome,preco,quantidade],
+        (err) => {
+            console.log(err)
+            if(err) return res.status(500).send("erro ao cadastrar produto");
+            res.status(200).send("produto cadastrado com sucesso");
+        
+        }
+    );
+};
+
+exports.listarProduto = (req,res) => {
+    conexao.query("SELECT * FROM produtos",(err,results) => {
+        if(err)return res.status(500).send("erro ao buscar produtos");
+        res.status(200).send(results)
+    })
+};
+
+exports.atualizarProduto = (req,res) => {
+    const{id} = req.params;
+    const{nome,preco} = req.body;
+    const query = "UPDATE produtos SET nome = ?, preco = ? where id = ? ";
+    conexao.query(query,[nome,preco,id],(err,results) => {
+        if(err) return res.status(500).send("erro ao atualizar");
+        if(results.affectedrows===0)return res.send(404).send("produto nao encontrado");
+            res.send("produto atualizado com sucesso");
+    });
+};
+
+exports.deletarProduto = (req, res) => {
+    const { id } = req.params;
+  
+    const query = 'DELETE FROM produtos WHERE id = ?';
+
+
+    const values = [id];
+  
+    conexao.query(query, values, (err, results) => {
+      if (err) {
+        return res.status(500).json({ mensagem: 'Erro ao deletar o produto', erro: err });
+      }
+  
+      if (results.affectedRows === 0) {
+        return res.status(404).json({ mensagem: 'Produto não encontrado' });
+      }
+  
+      return res.status(200).json({ mensagem: 'Produto deletado com sucesso' });
+    });
+  };
+  
+ 
+
+  exports.criarFuncionarios = (req,res) => {
+    const {nome,cargo,idade} = req.body;
+
+    conexao.query(
+        "INSERT INTO funcionarios (nome,cargo, idade) VALUES (?,?,?)",
+        [nome,cargo,idade],
+        (err) => {
+            console.log(err)
+            if(err) return res.status(500).send("erro ao cadastrar funcionario");
+            res.status(200).send("Funcionario cadastrado com sucesso");
+        
+        }
+    );
+};
+
+exports.listarFuncionarios = (req,res) => {
+    conexao.query("SELECT * FROM funcionarios",(err,results) => {
+        if(err)return res.status(500).send("erro ao buscar funcionarios ");
+        res.status(200).send(results)
+    })
+};
+
+exports.atualizarFuncionarios = (req,res) => {
+    const{id} = req.params;
+    const{nome,preco} = req.body;
+    const query = "UPDATE produtos SET nome = ?, cargo = ?, idade =? where id = ? ";
+    conexao.query(query,[nome,preco,id],(err,results) => {
+        if(err) return res.status(500).send("erro ao atualizar");
+        if(results.affectedrows===0)return res.send(404).send("produto nao encontrado");
+            res.send("produto atualizado com sucesso");
+    });
+};
+
+
+exports.deletarFuncionarios = (req, res) => {
+    const { id } = req.params;
+  
+    const query = 'DELETE FROM funcionarios WHERE id = ?';
+    const values = [id];
+  
+    conexao.query(query, values, (err, results) => {
+      if (err) {
+        return res.status(500).json({ mensagem: 'Erro ao deletar o Funcionario', erro: err });
+      }
+  
+      if (results.affectedRows === 0) {
+        return res.status(404).json({ mensagem: 'Funcionario não encontrado' });
+      }
+  
+      return res.status(200).json({ mensagem: 'Funcionario deletado com sucesso' });
+    });
+  };
+  
